e5f102558aec4d74f1eef77a0bc4c947d52b0517
paytonrules <esmith@8thlight.com>
2013-08-17 09:55:23 -0500

    Show thumbnails as if they exist

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,35 +1,67 @@
 package controllers
 
 import (
-  "github.com/robfig/revel"
-  "path/filepath"
-  "os"
+	//	"github.com/nfnt/resize"
+	"github.com/robfig/revel"
+	//  "image/jpeg"
+	//  "image/png"
+	"os"
+	"path/filepath"
 )
+
+type Directory struct {
+	FullName  string
+	ShortName string
+}
+
+type Image struct {
+	Thumbnail string
+	FullPath  string
+}
 
 type App struct {
 	*revel.Controller
 }
 
+func thumbnailPathFor(imagePath string) string {
+  return filepath.Dir(imagePath) + "/thumbnails/" + filepath.Base(imagePath)
+}
+
+func makeImage(imagePath string) Image {
+  return Image{Thumbnail: thumbnailPathFor(imagePath), FullPath: imagePath}
+}
+
+func (c App) renderDirectory(directory string) revel.Result {
+	allFiles, error := filepath.Glob(directory + "/*")
+	if error != nil {
+		return c.Render(error)
+	}
+
+	directories := make([]Directory, 0, len(allFiles))
+	files := make([]Image, 0, len(allFiles))
+
+	for i := range allFiles {
+		lstat, error := os.Lstat(allFiles[i])
+		if error != nil {
+			return c.Render(error)
+		}
+
+		if lstat.IsDir() {
+			directories = append(directories, Directory{FullName: allFiles[i], ShortName: filepath.Base(allFiles[i])})
+		} else {
+			files = append(files, makeImage(allFiles[i]))
+		}
+	}
+	c.RenderArgs["files"] = files
+	c.RenderArgs["directories"] = directories
+	return c.RenderTemplate("App/Show.html")
+}
+
+func (c App) Show(directory string) revel.Result {
+	return c.renderDirectory(directory)
+}
+
 func (c App) Index() revel.Result {
-  allFiles, error := filepath.Glob("public/events/*")
-  if error != nil {
-    return c.Render(error)
-  }
-
-  directories := make([]string, 0, len(allFiles))
-  files := make([]string, 0, len(allFiles))
-
-  for i := range allFiles {
-    lstat, error := os.Lstat(allFiles[i])
-    if error != nil {
-      return c.Render(error)
-    }
-
-    if lstat.IsDir() {
-      directories = append(directories, allFiles[i])
-    } else {
-      files = append(files, allFiles[i])
-    }
-  }
-  return c.Render(files, directories)
+	// Convert to a url
+	return c.renderDirectory("public/events")
 }
