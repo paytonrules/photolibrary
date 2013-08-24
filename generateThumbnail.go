917e2cdaf46ee03c82b780005773e1f1bcc669c5
paytonrules <esmith@8thlight.com>
2013-08-24 10:52:17 -0500

    Can generate thumbnails for JPG

---
+++ generateThumbnail.go
@@ -0,0 +1,58 @@
+package image
+
+import (
+	"github.com/nfnt/resize"
+	"log"
+	"image/jpeg"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// Make work with PNG
+// Clean up error handling
+func (image *Image) GenerateThumbnail() {
+	_, err := os.Lstat(image.Thumbnail)
+	if err != nil && os.IsNotExist(err) {
+		if strings.Contains(strings.ToUpper(filepath.Ext(image.FullPath)), ".JPG") {
+      // touch file at very beginning to reduce duplicate jobs
+      // Still a race condition, but as long as there are no errors I can live with it
+      // Open the full image file
+      file, err := os.Open(image.FullPath)
+      if err != nil {
+        log.Println(err)
+      }
+
+      // decode jpeg into image.Image
+      img, err := jpeg.Decode(file)
+      if err != nil {
+        log.Println(err)
+      }
+      file.Close()
+
+      // See if there is a thumbnails directory
+      _, err = os.Lstat(filepath.Dir(image.Thumbnail))
+      if err != nil {
+        if os.IsNotExist(err) {
+          os.Mkdir(filepath.Dir(image.Thumbnail), os.ModeDir|os.ModePerm)
+        } else {
+          log.Println(err)
+        }
+      }
+
+      // Probably shouldn't continue in the event of error but.....
+      // resize to width 200 using NearestNeighbor resampling
+      // and preserve aspect ratio
+      m := resize.Resize(200, 0, img, resize.NearestNeighbor)
+
+      out, err := os.Create(image.Thumbnail)
+      if err != nil {
+        log.Println(err)
+      }
+      defer out.Close()
+
+      // write new image to file
+      jpeg.Encode(out, m, nil)
+		}
+	}
+}
---
+++ image.go
@@ -0,0 +1,14 @@
+package image
+
+type Image struct {
+  Thumbnail string
+	FullPath  string
+}
+
+func NewImage(thumbnail, fullpath string) *Image {
+  return &Image{Thumbnail: thumbnail, FullPath: fullpath};
+}
+
+func CopyImage(image Image) *Image {
+  return &Image{Thumbnail: image.Thumbnail, FullPath: image.FullPath};
+}
