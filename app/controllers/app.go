4bdf933dde005cb562e44b359ab44568880fb805
paytonrules <esmith@8thlight.com>
2013-08-31 21:35:52 -0500

    Use a config for the directory and URL

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -1,98 +1,30 @@
 package controllers
 
 import (
-	"github.com/paytonrules/image"
 	"github.com/paytonrules/photoLibrary/app/models"
 	"github.com/robfig/revel"
-	"path/filepath"
 )
-
-type Directory struct {
-	FullName  string
-	ShortName string
-}
 
 type App struct {
 	*revel.Controller
-}
-
-func thumbnailPathFor(imagePath string) string {
-	return filepath.Dir(imagePath) + "/.thumbnails/" + filepath.Base(imagePath)
-}
-
-func makeImage(imagePath string) image.Image {
-	return image.Image{Thumbnail: thumbnailPathFor(imagePath), FullPath: imagePath}
-}
-
-func removeHiddenFilesFrom(allFiles []string) []string {
-	filesWithoutHiddenFiles := make([]string, 0, len(allFiles))
-	for _, filename := range allFiles {
-		if filepath.Base(filename)[0] != '.' {
-			filesWithoutHiddenFiles = append(filesWithoutHiddenFiles, filename)
-		}
-	}
-
-	return filesWithoutHiddenFiles
-}
-
-func findEligibleFiles(glob string) ([]string, error) {
-	eligibleFiles, error := filepath.Glob(glob)
-	return removeHiddenFilesFrom(eligibleFiles), error
 }
 
 // Show a place holder for images that don't have thumbnails yet
 // Show an error for invalid files (I don't know how to do this thingy)
 // Support .thm and movies
 func (c App) renderDirectory(directory string) revel.Result {
-	/*
-		filesWithoutHiddenFiles, error := findEligibleFiles(directory + "/*")
-
-		if error != nil {
-			return c.RenderError(error)
-		}
-
-		directories := make([]Directory, 0, len(filesWithoutHiddenFiles))
-		images := make([]image.Image, 0, len(filesWithoutHiddenFiles))
-
-		for _, currentFile := range filesWithoutHiddenFiles {
-			lstat, error := os.Lstat(currentFile)
-			if error != nil {
-				return c.RenderError(error)
-			}
-
-			if lstat.IsDir() {
-				directories = append(directories, Directory{FullName: currentFile, ShortName: filepath.Base(currentFile)})
-			} else {
-				images = append(images, makeImage(currentFile))
-			}
-		}
-
-		jobs.Now(photoJobs.GenerateThumbnails{Images: images})
-
-		imagesWithTemporaryThumbnails := make([]image.Image, 0, len(images))
-		for _, currentImage := range images {
-			_, error := os.Lstat(currentImage.Thumbnail)
-
-			if os.IsNotExist(error) {
-				// You should use 'images url' or whatever the revel syntax is
-				imagesWithTemporaryThumbnails = append(imagesWithTemporaryThumbnails,
-					*image.NewImage("public/img/thumbnail_being_generated.png", currentImage.FullPath))
-			} else {
-				imagesWithTemporaryThumbnails = append(imagesWithTemporaryThumbnails,
-					*image.CopyImage(currentImage))
-			}
-		}
-
-		c.RenderArgs["images"] = imagesWithTemporaryThumbnails
-		c.RenderArgs["directories"] = directories
-		return c.RenderTemplate("App/Show.html")*/
+  image_url, found := revel.Config.String("image_url")
+  if !found {
+    return c.RenderText("Could not find image url")
+  }
 	events := models.FileSystemEvents{}
 	event, err := events.Find(directory)
-  revel.INFO.Println(event)
+	revel.INFO.Println(event)
 
 	if err != nil {
 		return c.RenderError(err)
 	} else {
+    c.RenderArgs["root_url"] = image_url
 		c.RenderArgs["images"] = event.Images
 		c.RenderArgs["directories"] = event.Events
 		return c.RenderTemplate("App/Show.html")
@@ -104,5 +36,11 @@ func (c App) Show(directory string) revel.Result {
 }
 
 func (c App) Index() revel.Result {
-	return c.renderDirectory("public/events")
+  rootDir, found := revel.Config.String("root_dir")
+  if found {
+    return c.renderDirectory(rootDir)
+  } else {
+    return c.RenderText("Could not find root directory")
+  }
+
 }
