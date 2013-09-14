66000e03b42ae6027af0ea94a3a1abe1f0c20987
Eric Smith <esmith@8thlight.com>
2013-09-14 08:38:44 -0500

    Dont show unsupported files

--- app/controllers/app.go
+++ app/controllers/app.go
@@ -14,7 +14,7 @@ func (c App) renderDirectory(directory string) revel.Result {
   if !found {
     return c.RenderText("Could not find image url")
   }
-	events := photolibrary.FileSystemEvents{}
+	events := photolibrary.MakeFileSystemEvents([]string{".jpg", ".png", ".mov", ".avi"})
 	event, err := events.Find(directory)
 
   rootDir, _ := revel.Config.String("root_dir")
